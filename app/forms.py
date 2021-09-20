0a4844d8e2d1820fe627e863d97c797db4d04682
Paul Ndambo <paulkadabo@gmail.com>
2021-09-20 08:53:09 +0300

    files with new templates

---
+++ app/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from . models import Agent, TeamLeader, Invoice
+
+class AddAgentForm(forms.ModelForm):
+    class Meta:
+        model = Agent
+        fields = ('user', 'name', 'ssdc_number', 'project')
+
+        widgets = {
+        	'user': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'ssdc_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'project': forms.Select(attrs={'class': 'form-control'}),
+                      
+        }
+
+class AddTeamLeaderForm(forms.ModelForm):
+    class Meta:
+        model = TeamLeader
+        fields = ('user', 'name', 'ssdc_number', 'project')
+
+        widgets = {
+        	'user': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'ssdc_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'project': forms.Select(attrs={'class': 'form-control'}),
+                      
+        }
+
+class AddInvoiceForm(forms.ModelForm):
+    class Meta:
+        model = Invoice
+        fields = ('agent', 'isp_name', 'monthly_subscription', 'invoice_file')
+
+        widgets = {
+            'agent': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'agent', 'type': 'hidden'}),
+            'isp_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'monthly_subscription': forms.NumberInput(attrs={'class': 'form-control'}),
+            'invoice': forms.FileInput(attrs={'class': 'form-control'}),
+                      
+        }
