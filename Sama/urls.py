d17cd79680815f9a931a192656ae53f14cdf2066
Paul Ndambo <paulkadabo@gmail.com>
2021-09-27 13:39:05 +0300

    changes

--- Sama/urls.py
+++ Sama/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("app.urls")),
+    path("", include("data.urls")),
     path("", include("authentication.urls")),
     path('admin/', admin.site.urls),
 ]
